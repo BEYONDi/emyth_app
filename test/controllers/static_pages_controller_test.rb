6d9685d57b98acca3927de8b19daf90378819db8
nbiondi <natebiondi@gmail.com>
2014-11-11 00:57:05 -0800

    Added integration tests and completed navigation items.  Began new user signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Emyth Interview App"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Emyth Interview App"
+  end
 end
